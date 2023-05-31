185968d94d96ae1d22ab3aa96b6b6b76c651c30b
Dan Kolz <dan@emergentideas.com>
2023-05-31 14:40:16 -0500

    working service

---
+++ abstract-data-service.js
@@ -0,0 +1,279 @@
+const filog = require('filter-log')
+const addCallbackToPromise = require('add-callback-to-promise')
+const crypto = require("crypto");
+
+
+class DataService {
+
+	/**
+	 * 
+	 * @param {object} options
+ 	 * @param {string} [options.serviceName] Sets the name of this service for logging, and possibly other purposes
+ 	 * @param {boolean} [options.useIndependentIds] If true, records will get unique ID strings which are not tied to the underylying datastore
+ 	 * @param {object} options.collections an object holding the MongoDB collections this service will use, keyed by collection name
+ 	 * @param {object} [options.collections.default] the default collection. Technically optional, but the basic functions which
+	 * don't require the caller to specify the collection won't work if not set.
+	 * @param {EventEmitter} [options.notification] An EventEmitter that will be notified on create, update, and delete. The notification is:
+	 * emit('object-change', { the object }, changeType: create, update, delete)
+	 * @param {string} [options.eventName] The event name which will be used for the emitter. By default this is 'object-change'.
+	 * 
+	 */
+	constructor({serviceName = 'dataService', useIndependentIds = true, eventName = 'object-change'} = {}) {
+		Object.assign(this, {
+			log: filog(serviceName + ':')
+			, collections: []
+			, useIndependentIds: useIndependentIds
+			, eventName: eventName
+		}, arguments[0])
+	}
+	
+	/**
+	 * Generates storage system independent random ids
+	 * @returns a base64 string, 256 bits of randomness
+	 */
+	generateId() {
+		return crypto.randomBytes(32).toString("base64");
+	}
+	
+	/**
+	 * Finds documents via a MongoDB query object
+	 * @param {object} query 
+	 * @returns An array of resultant objects, possibly empty.
+	 */
+	async fetch(query) {
+		return this._fetchByQuery(this.collections.default, query)
+	}
+	
+	/**
+	 * Fetch a single document.
+	 * @param {*} id A query or a string which is the id
+	 * @returns Either a single document or null
+	 */
+	async fetchOne(id) {
+		let items = await this._fetchById(this.collections.default, id)
+		if(items && items.length > 0) {
+			return items[0]
+		}
+		return null
+	}
+	/**
+	 * Store a document. It will replace or insert if there's no _id member
+	 * @param {*} focus 
+	 * @returns A promise that resolves to the saved object and the native mongo result
+	 * in an result object [obj, native]
+	 */
+	async save(focus) {
+		return this._save(this.collections.default, focus) 
+	}
+	/**
+	 * Stores an array of documents. Returns an array of promises.
+	 * @param {array} foci 
+	 * @returns 
+	 */
+	saveMany(foci) {
+		return this._saveMany(this.collections.default, foci) 
+	}
+	/**
+	 * Removes one of more documents. This assumes the id is actually just an id,
+	 * but will work fine if a broader query is passed.
+	 * @param {*} id A query or a string which is the id
+	 * @returns 
+	 */
+	async remove(id) {
+		return this._removeByQuery(this.collections.default, this.createIdQuery(id))
+	}
+
+	/**
+	 * Creates an object to query the db by an object's ID
+	 * @param {*} id 
+	 * @returns 
+	 */
+	createIdQuery(id) {
+		if(Array.isArray(id)) {
+			return id.map(this.createIdQuery)
+		}
+		if(typeof id === 'string') {
+			return {
+				_id: id
+				, id: id
+			}
+		}
+		return id
+	}
+
+	/**
+	 * Transforms the results of fetches. This is sometimes done when the object from the database should be augmented
+	 * with additional information or should be converted into an object with a specific class. Override this function
+	 * at need. By default it does essentially nothing.
+	 * @param {object[]} result An array of objects from the database
+	 * @param {string} collectionName The name of the collection these objects came from. If this class only queries a single
+	 * collection, this parameter won't be of much use. If it queries multiple collection, this will help inform the method
+	 * what to do with the object.
+	 * @returns A promise which results in an array of objects.
+	 */
+	async postFetchesProcessor(result, collectionName) {
+		return new Promise((resolve, reject) => {
+			resolve(result)
+		})
+	}
+
+	/**
+	 * The internal implementation of fetching objects
+	 * @param {object} collection Could be anyting. An array, a mongo collection, even just a string to 
+	 * identify what underlying datastore to use
+	 * @param {object|array} query An object in the mongodb query style, or an array of those objects
+	 * @returns a promise which resolves to the result of the fetch, generally an array of result objects.
+	 */
+	async _doInternalFetch(collection, query) {
+		throw new Error('_doInternalFetch not implemented')
+		/*
+			// Code will look something like this:
+
+			return new Promise((resolve, reject) => {
+				resolve([{}])
+			})
+		*/
+	}
+	
+	/**
+	 * Fetches a list of documents from the collection.
+	 * @param {Collection} collection A MongoDB Collection
+	 * @param {object,array} query A mongodb query object. Can me null or {} if you want all documents. 
+	 * @param {function} callback (optional) A callback if that's how you get down. This function would normally be used with promises and await.
+	 * @returns A promise which resolves to an array of documents
+	 */
+	_fetchByQuery(collection, query = {}, callback) {
+		let p = new Promise((resolve, reject) => {
+			this._doInternalFetch(collection, query).then(result => {
+				this.postFetchesProcessor(result, collection.collectionName).then((processed) => {
+					resolve(processed)
+				})
+			}).catch(err => {
+				this.log.error(err)
+				return reject(err)
+			})
+		})
+		return addCallbackToPromise(p, callback)
+	}
+	
+	/**
+	 * EXTENDING CLASSES MUST IMPLEMENT THIS METHOD.
+	 * The internal implementation of removing objects based on a query.
+	 * @param {object} collection Could be anyting. An array, a mongo collection, even just a string to 
+	 * identify what underlying datastore to use
+	 * @param {object|array} query An object in the mongodb query style, or an array of those objects
+	 * @returns a promise which resolves to the result of the delete, generally an internal result object
+	 */
+	async _doInternalRemove(collection, query) {
+		throw new Error('_doInternalRemove not implemented')
+		/*
+			// Code will look something like this:
+
+			return new Promise((resolve, reject) => {
+				resolve(query)
+			})
+		*/
+	}
+	
+	_removeByQuery(collection, query = {}, callback) {
+		let p = new Promise((resolve, reject) => {
+			this._doInternalRemove(collection, query).then(result => {
+				this._notify(query, 'delete')
+				resolve(result)
+			}).catch(err => {
+				this.log.error(err)
+				return reject(err)
+			})
+		})
+		return addCallbackToPromise(p, callback)
+	}
+
+	/**
+	 * Fetches a list of documents from the collection.
+	 * 
+	 * @param {Collection} collection A MongoDB Collection
+	 * @param {*} id A query. If it's an object, that's used directly. If it's a string, we'll try to turn it into a MongoDB style ID query object.
+	 *  If an array is passed, each element is assumed to be an id of the objects to fetch.
+	 * @param {function} callback (optional) A callback if that's how you get down. This function would normally be used with promises and await.
+	 * @returns A promise which resolves to an array of documents containing either one of zero items
+	 */
+	_fetchById(collection, id, callback) {
+		return this._fetchByQuery(collection, this.createIdQuery(id), callback)
+	}
+	
+
+	/**
+	 * EXTENDING CLASSES MUST IMPLEMENT THIS METHOD.
+	 * The internal implementation of saving objects, either insert or update. 
+	 * @param {object} collection Could be anyting. An array, a mongo collection, even just a string to 
+	 * identify what underlying datastore to use
+	 * @param {object} focus An object to save
+	 * @returns a promise which resolves to the result of the save, an array of the [result, change-type(update,create), native-result].
+	 */
+	async _doInternalSave(collection, focus) {
+		throw new Error('_doInternalSave not implemented')
+		/*
+			// Code will look something like this:
+
+			let p = new Promise((resolve, reject) => {
+				return resolve([focus, 'update', null])
+			})
+			return p
+		*/
+	}
+
+	/**
+	 * Saves an object. If it already has an _id attribute, it replaces the existing document, otherwise inserts it.
+	 * @param {Collection} collection A MongoDB Collection
+	 * @param {object} focus The object to save
+	 * @param {function} callback (optional) A callback if that's how you get down. This function would normally be used with promises and await.
+	 * @returns Promise
+	 */
+	async _save(collection, focus, callback) {
+		let p = new Promise((resolve, reject) => {
+			if(this.useIndependentIds && !focus.id) {
+				focus.id = this.generateId()
+			}
+			this._doInternalSave(collection, focus).then(([saved, saveType, nativeResult]) => {
+				this._notify(saved, saveType)
+				return resolve([saved, nativeResult])
+			})
+			.catch(err => {
+				reject(err)
+			})
+		})
+		return addCallbackToPromise(p, callback)
+	}
+
+	
+	/**
+	 * Saves an array of objects. If the objects already have an _id attribute, it replaces the existing document, otherwise inserts it.
+	 * @param {Collection} collection A MongoDB Collection
+	 * @param {object[]} foci An array of objects to save
+	 * @param {function} callback (optional) A callback if that's how you get down. Called when Promise.all is done. This function would normally be used with promises and await.
+	 * @returns Array An array of promises which represent saves for each object in the array. If you want to wait on the results, try:
+	 * 		Promise.all(service._saveMany(col, items)).then(result => {
+	 * 			// some code
+	 * 		})
+	 * 	or
+	 * 		await Promise.all(service._saveMany(col, items))
+	 */
+	_saveMany(collection, foci, callback) {
+		let promises = []
+		for(let focus of foci) {
+			promises.push(this._save(collection, focus))
+		}	
+		if(callback) {
+			addCallbackToPromise(Promise.all(promises), callback)
+		}	
+		return promises
+	}
+	
+	_notify(obj, type) {
+		if(this.notification) {
+			this.notification.emit(this.eventName, obj, type)
+		}
+	}
+}
+
+module.exports = DataService
---
+++ test/operations-test.js
@@ -0,0 +1,199 @@
+require('mocha')
+var expect = require('chai').expect
+var assert = require('chai').assert
+const tu = (one, two) => one * two
+const EventEmitter = require('events')
+
+const DataService = require('../abstract-data-service')
+
+function show(dat) {
+	console.log(JSON.stringify(dat, null, '\t'))
+
+}
+
+class DataServiceMock extends DataService {
+	constructor(options) {
+		super(options)
+	}
+	async _doInternalFetch(collection, query) {
+		return new Promise(async (resolve, reject) => {
+			if(Array.isArray(query)) {
+				let result = []
+				try {
+					for(let subquery of query) {
+						let subresult = await this._doInternalFetch(collection, subquery)
+						result.push(...subresult)
+					}
+				}
+				catch(e) {
+					return reject(e)
+				}
+				return resolve(result)
+			}
+			
+			if(!query || Object.keys(query).length == 0) {
+				return resolve([...collection])
+			}
+			let result = collection.filter(item => item._id == query._id || item.id == query.id)
+			resolve(result)
+		})
+	}
+
+	async _doInternalSave(collection, focus) {
+		let p = new Promise(async (resolve, reject) => {
+			let type = 'update'
+			if(!focus._id) {
+				focus._id = this.generateId()
+				type = 'create'
+			}
+			else {
+				await this._doInternalRemove(collection, focus)
+			}
+			collection.push(focus)
+			return resolve([focus, type, null])
+		})
+		return p
+	}
+	async _doInternalRemove(collection, query) {
+		return new Promise((resolve, reject) => {
+			for(let i = 0; i < collection.length; i++) {
+				if(collection[i]._id == query._id || collection[i].id == query.id) {
+					collection.splice(i, 1)
+					break;
+				}
+			}
+
+			resolve(query)
+		})
+	}
+}
+
+describe("basic data operations", async function () {
+
+	it("independent ids", function () {
+		let col = []
+		col.collectionName = 'default'
+		serv = new DataService({
+			collections: {
+				default: col
+			}
+		})
+		assert.equal(serv.useIndependentIds, true)
+		
+		let id = serv.generateId()
+		assert.isNotNull(id)
+
+		serv = new DataService({
+			collections: {
+				default: col
+			}
+			, useIndependentIds: false
+		})
+		assert.equal(serv.useIndependentIds, false)
+
+	})
+
+	it("ops", async function () {
+		let p = new Promise(async (resolve, reject) => {
+			try {
+				let col = []
+				col.collectionName = 'default'
+				let events = new EventEmitter()
+				serv = new DataServiceMock({
+					collections: {
+						default: col
+					}
+					, notification: events
+				})
+				
+				events.on('object-change', (one, two) => {
+					console.log(`object change: ${JSON.stringify(one)} ${two}`)
+				})
+
+				let dat = {
+					msg: 'hello'
+				}
+				let [r] = await serv.save(Object.assign({}, dat))
+				assert.isNotNull(r._id)
+				// Make sure we have an independent id
+				assert.isNotNull(r.id)
+				let id = r._id
+				let id2 = r.id
+
+				let result = await serv.fetch()
+				assert.equal(result.length, 1)
+
+				result = await serv.fetchOne(id)
+				assert.equal(result.msg, 'hello')
+
+				result = await serv.fetchOne(id.toString())
+				assert.equal(result.msg, 'hello')
+				
+				result.msg = 'hi'
+				await serv.save(result)
+				
+				result = await serv.fetchOne(id.toString())
+				assert.equal(result.msg, 'hi')
+
+				result = await serv.fetchOne({id: id2})
+				assert.equal(result.msg, 'hi')
+
+				result = await serv.fetchOne(id2)
+				assert.equal(result.msg, 'hi')
+
+				result = await serv.remove(id.toString())
+
+				result = await serv.fetchOne(id.toString())
+				assert.isNull(result)
+				
+				
+				let promises = serv.saveMany([
+					{msg: 'hello'}
+					, {msg: 'world'}
+				])
+				await Promise.all(promises)
+
+				result = await serv.fetch()
+				assert.equal(result.length, 2)
+				
+				let ids = result.map(item => item.id)
+				let ids2 = result.map(item => item._id.toString())
+				
+				result = await serv.fetch({})
+				assert.equal(result.length, 2)
+				
+				result = await serv.fetchOne(ids)
+				assert.isNotNull(result)
+
+				result = await serv.fetchOne(ids2)
+				assert.isNotNull(result)
+				
+				result = await serv.fetch(serv.createIdQuery(ids))
+				assert.equal(result.length, 2)
+
+				result = await serv.fetch(serv.createIdQuery(ids2))
+				assert.equal(result.length, 2)
+				
+				result = await serv.fetch({name: 'Kolz'})
+				assert.equal(result.length, 0)
+
+
+				// with independent ids turned off
+				serv.useIndependentIds = false
+				let native
+				[r, native] = await serv.save({msg: 'world'})
+				assert.isNotNull(r._id)
+				// Make sure we don't have an independent id
+				assert.isUndefined(r.id)
+				
+
+			}
+			catch(e) {
+				console.log(e)
+				return reject('error')
+			}
+			resolve()
+		})
+		return p
+	})
+})
